452703ec84f7d6687c17e159ada1ea14bd26063a
Mateusz Nowak <mateusz.nowak@xsolve.pl>
2014-02-27 21:30:34 +0100

    First release

---
+++ web/index.php
@@ -0,0 +1,145 @@
+<?php
+
+/**
+ * Require autoload file
+ */
+require_once '../vendor/autoload.php';
+
+use Silex\Application;
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\ArrayAdapter;
+use Macedigital\Silex\Provider\SerializerProvider;
+use Symfony\Component\HttpFoundation\Request;
+use Buzz\Browser;
+use Silex\Provider\TwigServiceProvider;
+use FranMoreno\Silex\Provider\PagerfantaServiceProvider;
+
+$app = new Application();
+$app['debug'] = true;
+
+$app->register(new \Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new SerializerProvider);
+$app->register(new TwigServiceProvider, array(
+    'twig.path' => __DIR__ . '/../views',
+));
+$app->register(new PagerfantaServiceProvider);
+$app->register(new Sfk\Silex\Provider\MongoDBServiceProvider(), array(
+    'mongodb.server' => 'mongodb://localhost:27017/sluchajse',
+    'mongodb.options' => []
+));
+
+$app->get('/', function(Request $request) use ($app) {
+    /** @var MongoClient $mongo */
+    $mongo = $app['mongodb'];
+
+    $lastTracks = $mongo->selectCollection('tracks')->find()->sort(['createdAt' => -1])->limit(10);
+    $topTracks = $mongo->selectCollection('tracks')->find()->sort(['listen' => -1])->limit(10);
+
+    return $app['twig']->render('index.html.twig', [
+        'lastTracks' => $lastTracks,
+        'topTracks' => $topTracks
+    ]);
+});
+
+$app->get('/mp3/{slug}', function(Request $request, $slug) use ($app) {
+    $id = (Integer) $slug;
+
+    /** @var MongoClient $mongo */
+    $mongo = $app['mongodb'];
+
+    $mongo->selectCollection('tracks')->update([
+        '_id' => $id
+    ], [
+        '$inc' => [
+            'listen' => 1
+        ]
+    ]);
+
+    $track = $mongo->selectCollection('tracks')->findOne([
+        '_id' => $id
+    ]);
+
+    return $app['twig']->render('show.html.twig', [
+        'track' => $track,
+        'slug' => $slug
+    ]);
+});
+
+$app->get('/last', function(Request $request) use ($app) {
+    /** @var MongoClient $mongo */
+    $mongo = $app['mongodb'];
+
+    $searches = $mongo->selectCollection('search')->find()->sort([ 'createdAt' => -1 ])->limit(200);
+
+    return $app['twig']->render('last.html.twig', [
+        'searches' => $searches
+    ]);
+});
+
+$app->get('/search', function(Request $request) use ($app) {
+    /** @var MongoClient $mongo */
+    $mongo = $app['mongodb'];
+
+    $browser = new Browser();
+    $serializer = $app['serializer'];
+
+    $q = $request->query->get('search');
+    $page = $request->query->get('page') ? (Integer) $request->query->get('page'): 1;
+
+    if (strlen($q) < 3) {
+        throw new \InvalidArgumentException('Search query parameters must be greater than 2 chars');
+    }
+
+    $query = str_replace('+', '-', urlencode($q));
+
+    $mongo->selectCollection('search')->update(['search' => $q], [
+        '$set' => [
+            'search' => $q,
+            'createdAt' => new \Datetime()
+        ]
+    ], ['upsert' => true]);
+
+    $response = $browser->get('http://www.mp3poisk.net/' . $query . '/' . $page, [
+        "Content-Type: application/json",
+        "X-Requested-With: XMLHttpRequest"
+    ]);
+
+    try {
+        $result = $serializer->deserialize($response->getcontent(), 'array', 'json');
+
+        if (isset($result['error'])) {
+            throw new \RuntimeException('Not found');
+        }
+
+        foreach ($result['items'] as $track) {
+            $mongo->selectCollection('tracks')->update([
+                '_id' => $track['file_id']
+            ], [
+                '$set' => [
+                    'name' => sprintf('%s - %s', $track['artist']['name'], $track['title']),
+                    'slug' => sprintf('%d-%s-%s', $track['file_id'], $track['slug']['artist'], $track['slug']['track']),
+                    'download' => $track['links']['download'],
+                    'duration' => $track['duration'],
+                    'createdAt' => new \Datetime(),
+                    'listen' => 0
+                ]
+            ], ['upsert' => true]);
+        }
+
+        $adapter = new ArrayAdapter(range(1, $result['pagination']['total_items']));
+        $pagerfanta = new Pagerfanta($adapter);
+        $pagerfanta->setMaxPerPage(20);
+        $pagerfanta->setCurrentPage($request->query->get('page', 1));
+    } catch (\Exception $e) {
+        $result = [];
+        $pagerfanta = null;
+    }
+
+    return $app['twig']->render('search.html.twig', [
+        'result' => $result,
+        'query' => ucwords($q),
+        'pager' => $pagerfanta
+    ]);
+});
+
+$app->run();
---
+++ web/js/app.js
@@ -0,0 +1,76 @@
+$(function() {
+    var $playlistContainer = $('.playlist-container');
+    var $templatePlaylistTrack = $('#template-playlist');
+    var PLAYLIST_STORAGE_KEY = 'playlisttracks';
+
+    var updatePlaylist = function() {
+        var trackCollection = JSON.parse(localStorage.getItem(PLAYLIST_STORAGE_KEY));
+        var $tbody = $playlistContainer.find('table tbody');
+
+        $tbody.empty();
+
+        if (trackCollection) {
+            trackCollection.forEach(function(track) {
+                $tbody.append(_.template($templatePlaylistTrack.html(), track));
+            });
+        }
+    };
+
+    updatePlaylist();
+
+    $playlistContainer.find('.remove').bind('click', function(e) {
+        var item = localStorage.getItem(PLAYLIST_STORAGE_KEY);
+
+        if (item) {
+            var trackCollection = JSON.parse(localStorage.getItem(PLAYLIST_STORAGE_KEY));
+        } else {
+            return;
+        }
+
+        var trackId = $($(this).parents('tr')[0]).attr('data-id');
+
+        var tracks = trackCollection.filter(function(row) {
+            return trackId != row.id;
+        });
+
+        localStorage.setItem(PLAYLIST_STORAGE_KEY, JSON.stringify(tracks));
+
+        updatePlaylist();
+
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    $('.results-tracks .playlist').on('click', function(e) {
+        var $tr = $($(this).parents('tr')[0]);
+
+        if (localStorage.getItem(PLAYLIST_STORAGE_KEY)) {
+            var trackCollection = JSON.parse(localStorage.getItem(PLAYLIST_STORAGE_KEY));
+        } else {
+            var trackCollection = [];
+        }
+
+        var track = {
+            id: $tr.attr('data-id'),
+            title: $tr.find('.title a').html(),
+            url: $tr.find('.title a').attr('href')
+        };
+
+        var isAdded = trackCollection.filter(function(row) {
+            return track.id == row.id;
+        });
+
+        if (isAdded.length == 0) {
+            trackCollection.push(track);
+
+            localStorage.setItem(PLAYLIST_STORAGE_KEY, JSON.stringify(trackCollection));
+        }
+
+        $(this).remove();
+
+        updatePlaylist();
+
+        e.preventDefault();
+        e.stopPropagation();
+    });
+});
